19098cfbe86ef2a307bba555f20bfaa0d5ce3d43
alexrochatsi <alexrochatsi@gmail.com>
2022-01-10 21:48:52 -0300

    Validando mensagens das classes modelos.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar -Dspring.profiles.active=heroku build/libs/credito-tqi-0.0.1-SNAPSHOT.jar
--- src/main/java/com/tqi/teste/creditotqi/controller/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/tqi/teste/creditotqi/controller/exceptions/ResourceExceptionHandler.java
@@ -36,6 +36,4 @@ public class ResourceExceptionHandler {
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
-
-
 }
